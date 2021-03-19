613288098c858f1125823a3d482448a46083bbb2
Mahmud-Sufian <mahmud5281585@gmail.com>
2021-03-20 00:23:05 +0600

    almost complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,8 +15,8 @@ const Header = () => {
             <h2>Fast Riders</h2>
             <ul>
                 <li><Link to="/home">Home</Link></li>  
-                {/* <li><Link to="/destination/">Destination</Link></li> */}
-                <li><Link to="/home">Destination</Link></li> 
+                <li><Link to="/destination/">Destination</Link></li>
+                {/* <li><Link to="/home">Destination</Link></li>  */}
                 <li style={{color:'#fff'}}>{loggedInUser.name}</li> 
                 <li><Link to="/login"><button class="btn btn-primary" onClick={() => setLoggedInUser({})}>{loggedInUser.email ? 'Sign Out' : 'Sign In'}</button></Link></li> 
             </ul>
